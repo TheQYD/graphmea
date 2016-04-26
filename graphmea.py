3c136641f9632e0e59b3463ef4c248507d26f277
TheQYD <emailqasim@gmail.com>
2016-04-26 02:42:44 -0400

    -

--- graphmea.py
+++ graphmea.py
@@ -3,6 +3,7 @@
 import argparse
 import matplotlib.pyplot as plot
 import matplotlib.ticker as ticker
+import matplotlib.lines as lines
 import random
 
 COLORS = ['indigo', 'gold', 'hotpink', 'firebrick', 'indianred', 'yellow',
@@ -53,21 +54,21 @@ def RenderGraph(**args_dict):
   random_colors = args_dict.get('random_colors')
   xlabel = args_dict.get('xlabel')
   ylabel = args_dict.get('ylabel')
-  
+ 
   # Check for random colors
 
   if random_colors is True:
     random.shuffle(COLORS)
-  
+ 
   # Call the function based on the first argument.
   eval(graph_type.capitalize())(graph_data, args_dict)
-  
+ 
   # Find the axes maximums.
   graph_max = max([max(i) for i in graph_data])
   graph_len = max([len(i) for i in graph_data])
   #xmax = args_dict.get('xmax', graph_len - 1)
   #ymax = args_dict.get('ymax', round(graph_max * 1.05))
-  
+
   # Plot the graph.
   #plot.axis([0, xmax, 0, ymax])
   plot.xlabel(xlabel)
@@ -78,7 +79,7 @@ def RenderGraph(**args_dict):
 
 def Linegraph(graph_data, args_dict):
   graph_color = args_dict.get('graph_color', 'b')
-  
+
   for gd in graph_data:
     lg_color = COLORS[graph_data.index(gd)] 
     x = range(0, len(gd))
@@ -89,15 +90,25 @@ def Bargraph(graph_data, args_dict):
   width = .7/len(graph_data)
   xticks = args_dict.get('xticks')
   yticks = args_dict.get('yticks')
+  keys = args_dict.get('keys')
   graph_len = max([len(i) for i in graph_data])
   bar_pos = range(graph_len)
   tick_pos = [i + width/graph_len for i in bar_pos]
+  print keys
+  
 
   for gd in graph_data: 
     bg_color = COLORS[graph_data.index(gd)]
+    line = lines.Line2D(
+        [],
+        [],
+        color = bg_color,
+        label = keys[graph_data.index(gd)]
+    )
+    plot.legend(handles=[line])
     plot.bar(bar_pos, gd, width, color=bg_color, align='center')
     bar_pos = [i + width for i in bar_pos]
-    
+
   if xticks is not None:
     plot.xticks(tick_pos, xticks, rotation=45)
     #plot.yticks(range(len(gd)), gd)
@@ -116,19 +127,22 @@ if __name__ == '__main__':
   parser.add_argument('--ylabel', '-y', action="store", type=str)
   parser.add_argument('--xticks', action="store", type=str) 
   parser.add_argument('--yticks', action="store", type=str) 
-  
+  parser.add_argument('--keys', '-k', action="store", type=str) 
+
   args = parser.parse_args()
   graph_data = [map(int, i.split(',')) for i in args.data]
-  
+
   if args.xticks is not None:
     args.xticks = args.xticks.split(',')
   if args.yticks is not None:
     args.yticks = args.yticks.split(',')
+  if args.keys is not None:
+    args.keys = args.keys.split(',')
   if args.color is not None:
     args.color = [i.split(',') for i in args.color] 
   if args.style is not None:
     args.style = [i.split(',') for i in args.style] 
-  
+
   RenderGraph(
     graph_type=args.type,
     graph_data=graph_data,
@@ -140,5 +154,6 @@ if __name__ == '__main__':
     xlabel=args.xlabel,
     ylabel=args.ylabel,
     xticks=args.xticks,
+    keys=args.keys,
     graph_color=args.color
   )
